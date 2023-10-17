f2bb822b832ae598b8d21110d076074867f8cab8
iagomello17 <141081181+iagomello17@users.noreply.github.com>
2023-10-16 21:34:40 -0300

    Add files via upload

---
+++ exercico7.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+import math
+
+class Forma(ABC):
+    @abstractmethod
+    def calcular_area(self):
+        pass
+
+class Retangulo(Forma):
+    def __init__(self, comprimento, largura):
+        self.comprimento = comprimento
+        self.largura = largura
+
+    def calcular_area(self):
+        return self.comprimento * self.largura
+
+class Circulo(Forma):
+    def __init__(self, raio):
+        self.raio = raio
+
+    def calcular_area(self):
+        return math.pi * (self.raio ** 2)
+
+# Exemplo de uso:
+retangulo = Retangulo(5, 4)
+circulo = Circulo(3)
+
+print(f"Área do Retângulo: {retangulo.calcular_area()}")
+print(f"Área do Círculo: {circulo.calcular_area()}")
